4d8f515a414d4dc7855f5c108c6497297cdc0304
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-09-07 15:35:28 -0300

    feat: add LoginHeader component with welcome message and login prompt

---
+++ app/components/LoginPage/LoginFormContainer/LoginHeader/LoginHeader.tsx
@@ -0,0 +1,8 @@
+export const LoginHeader = () => {
+    return (
+        <header className='h-20 flex items-center justify-center flex-col'>
+            <span className='text-dark-10 font-archivo font-normal text-xl'>Bem-vindo(a) de volta</span>
+            <h1 className='text-dark-10 font-archivo font-medium text-3xl'>Faça login na sua conta</h1>
+        </header>
+    );
+};
