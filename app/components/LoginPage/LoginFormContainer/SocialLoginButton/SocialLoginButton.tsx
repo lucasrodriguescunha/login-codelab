f48871de5fddfe8408c8ed1664a7bf28def9a2bf
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-09-07 15:35:43 -0300

    feat: add PasswordInput component with empty structure

---
+++ app/components/LoginPage/LoginFormContainer/SocialLoginButton/SocialLoginButton.tsx
@@ -0,0 +1,9 @@
+import {Button} from '@/app/shared/Button';
+import {GoogleIcon} from '@/app/shared/icons/GoogleIcon';
+
+export const SocialLoginButton = () => {
+    return (
+        <>
+        </>
+    );
+};
