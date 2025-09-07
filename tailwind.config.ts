9dfa339793f689fc22319dcea6e0564871bb90c0
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-09-06 22:04:41 -0300

    feat: add Tailwind CSS configuration and update package.json for testing

---
+++ tailwind.config.ts
@@ -0,0 +1,30 @@
+import type {Config} from 'tailwindcss'
+
+const config: Config = {
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            background: {
+
+            },
+            colors: {
+                primary: 'var(--primary-color)',
+                secondary: 'var(--secondary-color)',
+                dark: {
+                    10: 'var(--dark-color-10)',
+                    20: 'var(--dark-color-20)',
+                    30: 'var(--dark-color-30)',
+                    40: 'var(--dark-color-40)',
+                },
+                white: 'var(--color-white)',
+            },
+        },
+    },
+    plugins: [],
+}
+
+export default config
