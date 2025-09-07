52ca117479a140d69bf226353b183bd3e69cfc1e
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-09-07 15:35:20 -0300

    feat: update layout to use Archivo font and change language to Portuguese

--- app/components/LoginPage/LoginFormContainer/LoginFormContainer.tsx
+++ app/components/LoginPage/LoginFormContainer/LoginFormContainer.tsx
@@ -1,7 +1,11 @@
+import {LoginHeader} from '@/app/components/LoginPage/LoginFormContainer/LoginHeader/LoginHeader';
+import {LoginForm} from '@/app/components/LoginPage/LoginFormContainer/LoginForm/LoginForm';
+
 export const LoginFormContainer = () => {
     return (
-        <>
-            <div>Hello world!</div>
-        </>
+        <div className='bg-white flex flex-col items-center justify-center h-screen px-8'>
+            <LoginHeader/>
+            <LoginForm/>
+        </div>
     );
 };
