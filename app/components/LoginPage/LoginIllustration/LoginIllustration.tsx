651f97354937bf9379bdd233845be7df319fc441
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-09-06 22:04:22 -0300

    feat: add LoginIllustration component for login page illustration

---
+++ app/components/LoginPage/LoginIllustration/LoginIllustration.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+
+export const LoginIllustration = () => {
+    return (
+        <>
+            <div className='bg-[var(--secondary-color)] h-screen flex items-center justify-center'>
+                <Image
+                    className='object-center'
+                    src='/illustration.png'
+                    alt='Imagem de ilustração'
+                    width={500}
+                    height={420}
+                />
+            </div>
+        </>
+    );
+}
