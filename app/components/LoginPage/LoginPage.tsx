943880fd32c89d068ab92fe48fa998e93b5e0ae5
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-09-06 22:03:58 -0300

    feat: implement LoginPage layout with illustration and form container

--- app/components/LoginPage/LoginPage.tsx
+++ app/components/LoginPage/LoginPage.tsx
@@ -1,7 +1,11 @@
+import {LoginIllustration} from "@/app/components/LoginPage/LoginIllustration/LoginIllustration";
+import {LoginFormContainer} from "@/app/components/LoginPage/LoginFormContainer/LoginFormContainer";
+
 export const LoginPage = () => {
     return (
-        <>
-
-        </>
+        <div className='grid grid-cols-2'>
+            <LoginIllustration/>
+            <LoginFormContainer/>
+        </div>
     );
 }
