8ef757c28849213e1244f10bcafc65e469a7573e
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-09-08 13:17:28 -0300

    fix: add padding and gap to LoginHeader component for improved layout

--- app/components/LoginPage/LoginFormContainer/LoginHeader/LoginHeader.tsx
+++ app/components/LoginPage/LoginFormContainer/LoginHeader/LoginHeader.tsx
@@ -1,6 +1,6 @@
 export const LoginHeader = () => {
     return (
-        <div className='w-sm h-20 flex flex-col justify-center'>
+        <div className='w-sm h-20 flex flex-col justify-center pb-10 gap-2'>
             <span className='text-[#1A202C] font-medium text-xl'>Bem-vindo(a) de volta</span>
             <h1 className='text-[#1A202C] font-semibold text-3xl'>Faça login na sua conta</h1>
         </div>
