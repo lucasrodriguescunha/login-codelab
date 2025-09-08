d154b7de6b8ba3b9d6a12905d6d2110336750fec
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-09-08 11:28:34 -0300

    fix: update InputField component styles for improved layout and appearance

--- app/shared/InputField.tsx
+++ app/shared/InputField.tsx
@@ -2,12 +2,12 @@ import {InputFieldProps} from '@/app/types';
 
 export const InputField = ({label, type, placeholder}: InputFieldProps) => {
     return (
-        <div className='flex flex-col gap-1'>
+        <div className='w-full flex flex-col gap-1'>
             <label className='text-sm font-medium'>{label}</label>
             <input
                 type={type}
                 placeholder={placeholder}
-                className='border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500'
+                className='bg-[#F7FAFC] w-full border rounded p-2 focus:outline-none focus:ring-2'
             />
         </div>
     );
