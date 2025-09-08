3cb89829f78990f2cd7dc4dacd2832a39a80fefc
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-09-08 11:29:10 -0300

    fix: update LoginHeader component styles for improved layout

--- app/components/LoginPage/LoginFormContainer/LoginHeader/LoginHeader.tsx
+++ app/components/LoginPage/LoginFormContainer/LoginHeader/LoginHeader.tsx
@@ -1,6 +1,6 @@
 export const LoginHeader = () => {
     return (
-        <div className='h-20 flex flex-col justify-center gap-1'>
+        <div className='w-sm h-20 flex flex-col justify-center'>
             <span className='text-[#1A202C] font-medium text-xl'>Bem-vindo(a) de volta</span>
             <h1 className='text-[#1A202C] font-semibold text-3xl'>Faça login na sua conta</h1>
         </div>
