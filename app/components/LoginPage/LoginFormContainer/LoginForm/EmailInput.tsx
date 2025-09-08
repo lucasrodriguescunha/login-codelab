e86a54afb10b640c7742c4b63f1024f8b7ba9b0d
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-09-07 22:42:44 -0300

    feat: add EmailInput component with labeled InputField for email input

--- app/components/LoginPage/LoginFormContainer/LoginForm/EmailInput.tsx
+++ app/components/LoginPage/LoginFormContainer/LoginForm/EmailInput.tsx
@@ -1,6 +1,13 @@
+import {InputField} from '@/app/shared/InputField';
+
 export const EmailInput = () => {
     return (
         <>
+            <InputField
+                label={'E-mail'}
+                type={'email'}
+                placeholder={'exemplo@gmail.com'}
+            />
         </>
     );
 };
