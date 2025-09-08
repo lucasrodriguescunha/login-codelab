81fb419a31ce29ba8a80af94b074fc5d552cf69e
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-09-07 22:30:56 -0300

    feat: update LoginForm to include labeled InputFields for email and password

--- app/components/LoginPage/LoginFormContainer/LoginForm/LoginForm.tsx
+++ app/components/LoginPage/LoginFormContainer/LoginForm/LoginForm.tsx
@@ -4,9 +4,16 @@ import {InputField} from "@/app/shared/InputField";
 export const LoginForm = () => {
     return (
         <form className='flex flex-col gap-6'>
-            <InputField/>
-            <InputField/>
-            <SocialLoginButton fullWidth/>
+            <InputField
+                label='E-mail'
+                type='email'
+                placeholder='exemplo@gmail.com'
+            />
+            <InputField
+                label='Senha'
+                type='password'
+                placeholder='0123456789'/>
+            <SocialLoginButton/>
         </form>
     );
 };
