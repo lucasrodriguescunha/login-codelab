a3e4e9afa78eeafa67f91e58b89c6fa78c0a11bf
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-09-07 22:42:54 -0300

    feat: replace InputField with EmailInput and PasswordInput in LoginForm

--- app/components/LoginPage/LoginFormContainer/LoginForm/LoginForm.tsx
+++ app/components/LoginPage/LoginFormContainer/LoginForm/LoginForm.tsx
@@ -1,19 +1,19 @@
+'use client'
+
 import {SocialLoginButton} from '@/app/components/LoginPage/LoginFormContainer/SocialLoginButton/SocialLoginButton';
-import {InputField} from "@/app/shared/InputField";
+import {EmailInput} from '@/app/components/LoginPage/LoginFormContainer/LoginForm/EmailInput';
+import {PasswordInput} from '@/app/components/LoginPage/LoginFormContainer/LoginForm/PasswordInput';
 
 export const LoginForm = () => {
     return (
         <form className='flex flex-col gap-6'>
-            <InputField
-                label='E-mail'
-                type='email'
-                placeholder='exemplo@gmail.com'
-            />
-            <InputField
-                label='Senha'
-                type='password'
-                placeholder='0123456789'/>
-            <SocialLoginButton/>
+            <EmailInput/>
+            <PasswordInput/>
+            <SocialLoginButton
+                provider={'google'}
+                onLogin={function (): void {
+                    console.log(`Login realizado com com sucesso!`);
+                }}/>
         </form>
     );
 };
