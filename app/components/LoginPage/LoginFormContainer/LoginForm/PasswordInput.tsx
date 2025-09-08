22c3418b3632e6ab406429ec036b0ce0b0748bdd
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-09-07 22:43:02 -0300

    feat: add InputField to PasswordInput component for password input

--- app/components/LoginPage/LoginFormContainer/LoginForm/PasswordInput.tsx
+++ app/components/LoginPage/LoginFormContainer/LoginForm/PasswordInput.tsx
@@ -1,6 +1,13 @@
+import {InputField} from '@/app/shared/InputField';
+
 export const PasswordInput = () => {
     return (
         <>
+            <InputField
+                label={'Senha'}
+                type={'password'}
+                placeholder={'0123456789'}
+            />
         </>
     );
 };
