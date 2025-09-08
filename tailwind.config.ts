9aba509cd785738a74f64802143d6956927d5488
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-09-07 22:31:13 -0300

    refactor: remove unused theme configurations from tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,30 +6,6 @@ const config: Config = {
         './components/**/*.{js,ts,jsx,tsx,mdx}',
         './app/**/*.{js,ts,jsx,tsx,mdx}',
     ],
-    theme: {
-        extend: {
-            colors: {
-                primary: 'var(--primary-color)',
-                secondary: 'var(--secondary-color)',
-                dark: {
-                    10: 'var(--dark-color-10)',
-                    20: 'var(--dark-color-20)',
-                    30: 'var(--dark-color-30)',
-                    40: 'var(--dark-color-40)',
-                },
-                white: 'var(--color-white)',
-            },
-            fontFamily: {
-                'archivo': ['var(--font-archivo)', 'sans-serif'],
-            },
-            width: {
-                '400': '400px',
-            },
-            height: {
-                '528': '528px',
-            },
-        },
-    },
     plugins: [],
 }
 
