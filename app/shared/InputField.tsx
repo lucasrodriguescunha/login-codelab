db28e8d4fb2953e0b57a5e9bd7219c0604060546
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-09-07 22:30:46 -0300

    feat: enhance InputField component with props for label, type, and placeholder

--- app/shared/InputField.tsx
+++ app/shared/InputField.tsx
@@ -1,8 +1,14 @@
-import {Input} from '@/app/shared/Input';
+import {InputFieldProps} from '@/app/types';
 
-export const InputField = () => {
+export const InputField = ({label, type, placeholder}: InputFieldProps) => {
     return (
-        <>
-        </>
+        <div className='flex flex-col gap-1'>
+            <label className='text-sm font-medium'>{label}</label>
+            <input
+                type={type}
+                placeholder={placeholder}
+                className="border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+        </div>
     );
 };
