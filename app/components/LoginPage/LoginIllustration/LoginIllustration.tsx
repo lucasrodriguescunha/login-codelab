95f6883a7123279467dd75f7e357415eee091b54
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-09-08 11:29:22 -0300

    fix: update background color in LoginIllustration component for better aesthetics

--- app/components/LoginPage/LoginIllustration/LoginIllustration.tsx
+++ app/components/LoginPage/LoginIllustration/LoginIllustration.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export const LoginIllustration = () => {
     return (
         <>
-            <div className='bg-[var(--secondary-color)] h-screen flex items-center justify-center'>
+            <div className='bg-[#F3FFF2] h-screen flex items-center justify-center'>
                 <Image
                     className='object-center'
                     src='/illustration.png'
