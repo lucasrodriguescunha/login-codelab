c8cd33f2e433ef246fe2da5d3778f45517ef9f9d
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-09-08 11:29:30 -0300

    fix: extend theme configuration in tailwind.config.ts for future customization

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,9 @@ const config: Config = {
         './components/**/*.{js,ts,jsx,tsx,mdx}',
         './app/**/*.{js,ts,jsx,tsx,mdx}',
     ],
+    theme: {
+        extend: {},
+    },
     plugins: [],
 }
 
