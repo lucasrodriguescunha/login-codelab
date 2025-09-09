9e0e4d1a55c8858d646118ecbcb1f7bf93c4f807
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-09-09 17:10:38 -0300

    fix: add SignInButton component for user authentication

---
+++ app/components/LoginPage/LoginFormContainer/SignInButton/SignInButton.tsx
@@ -0,0 +1,9 @@
+export const SignInButton = () => {
+    return (
+        <button
+            className='bg-[#1E9E6A] text-[#FFFFFF] text-lg font-semibold flex justify-center items-center h-12 border rounded p-4 focus:outline-none cursor-pointer'
+        >
+            Entrar na conta
+        </button>
+    );
+};
