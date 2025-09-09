fc748e548b5cd9df37ee6966e64f3e05cf200afc
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-09-09 16:44:44 -0300

    fix: add RememberMeCheckbox component for improved login experience

---
+++ app/components/LoginPage/LoginFormContainer/LoginForm/RememberMeCheckbox.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Link from 'next/link';
+
+export const RememberMeCheckbox = () => {
+    return (
+        <div className='flex items-center justify-between'>
+            <div className='flex items-center gap-2'>
+                <input
+                    type='radio'
+                    name='remember-option'
+                    id='remember-me'
+                    className='text-[#1E9E6A] font-medium cursor-pointer'
+                />
+                <label
+                    htmlFor='remember-me'
+                    className='text-[#1A202C] text-base font-medium'
+                >
+                    Lembre de mim
+                </label>
+            </div>
+            <Link
+                className='text-[#1E9E6A] text-base font-medium cursor-pointer'
+                href=''
+            >
+                Esqueceu sua senha?
+            </Link>
+        </div>
+    );
+};
