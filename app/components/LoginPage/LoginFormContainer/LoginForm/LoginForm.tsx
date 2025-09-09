1447b3a62c4e428509295d8729e3b7051192c7dd
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-09-09 16:44:27 -0300

    fix: add RememberMeCheckbox component to LoginForm for enhanced user experience

--- app/components/LoginPage/LoginFormContainer/LoginForm/LoginForm.tsx
+++ app/components/LoginPage/LoginFormContainer/LoginForm/LoginForm.tsx
@@ -3,12 +3,14 @@
 import {SocialLoginButton} from '@/app/components/LoginPage/LoginFormContainer/SocialLoginButton/SocialLoginButton';
 import {EmailInput} from '@/app/components/LoginPage/LoginFormContainer/LoginForm/EmailInput';
 import {PasswordInput} from '@/app/components/LoginPage/LoginFormContainer/LoginForm/PasswordInput';
+import {RememberMeCheckbox} from '@/app/components/LoginPage/LoginFormContainer/LoginForm/RememberMeCheckbox';
 
 export const LoginForm = () => {
     return (
         <form className='w-sm flex flex-col gap-6'>
             <EmailInput/>
             <PasswordInput/>
+            <RememberMeCheckbox/>
             <SocialLoginButton
                 provider={'google'}
                 onLogin={function (): void {
