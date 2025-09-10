dd1c640bef6e1244019c6f19b558c9defc22aa90
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-09-10 13:29:58 -0300

    fix: update LoginIllustration component to accept className prop for better styling flexibility

--- app/components/LoginPage/LoginIllustration/LoginIllustration.tsx
+++ app/components/LoginPage/LoginIllustration/LoginIllustration.tsx
@@ -1,16 +1,15 @@
 import Image from 'next/image';
+import {LoginIllustrationProps} from '@/app/types';
 
-export const LoginIllustration = () => {
+export const LoginIllustration = ({className}: LoginIllustrationProps) => {
     return (
-        <>
-            <div className='bg-[#F3FFF2] h-screen flex items-center justify-center'>
-                <Image
-                    src='/illustration.png'
-                    alt='Imagem de ilustração'
-                    width={500}
-                    height={420}
-                />
-            </div>
-        </>
+        <div className={`bg-[#F3FFF2] h-screen flex items-center justify-center ${className || ''}`}>
+            <Image
+                src='/illustration.png'
+                alt='Imagem de ilustração'
+                width={500}
+                height={420}
+            />
+        </div>
     );
 };
