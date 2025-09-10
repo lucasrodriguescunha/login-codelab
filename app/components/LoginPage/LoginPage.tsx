b00eb1d5ab9ff14a737423df062422f93148cccc
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-09-10 13:30:08 -0300

    fix: update LoginPage layout for responsive design and improve LoginIllustration visibility

--- app/components/LoginPage/LoginPage.tsx
+++ app/components/LoginPage/LoginPage.tsx
@@ -3,8 +3,8 @@ import {LoginFormContainer} from '@/app/components/LoginPage/LoginFormContainer/
 
 export const LoginPage = () => {
     return (
-        <div className='grid grid-cols-2'>
-            <LoginIllustration/>
+        <div className='grid grid-cols-1 lg:grid-cols-2'>
+            <LoginIllustration className='hidden lg:block'/>
             <LoginFormContainer/>
         </div>
     );
