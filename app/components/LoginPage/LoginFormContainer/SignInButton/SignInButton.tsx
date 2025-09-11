0d384ba5aea3a7e7e1f898c6c2f83b9392373e64
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-09-11 19:29:28 -0300

    fix: update SignInButton styles for improved text sizing on different screen sizes

--- app/components/LoginPage/LoginFormContainer/SignInButton/SignInButton.tsx
+++ app/components/LoginPage/LoginFormContainer/SignInButton/SignInButton.tsx
@@ -1,7 +1,7 @@
 export const SignInButton = () => {
     return (
         <button
-            className='bg-[#1E9E6A] text-[#FFFFFF] text-lg font-semibold flex justify-center items-center h-12 border rounded p-4 focus:outline-none cursor-pointer'
+            className='bg-[#1E9E6A] text-[#FFFFFF] text-base md:text-lg font-semibold flex justify-center items-center h-12 border rounded p-4 focus:outline-none cursor-pointer'
         >
             Entrar na conta
         </button>
