45961a34c2c10bc547b72f23ce4f8e4c981c4c89
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-09-11 19:29:14 -0300

    fix: enhance LoginPage layout for better responsiveness and full-height display

--- app/components/LoginPage/LoginPage.tsx
+++ app/components/LoginPage/LoginPage.tsx
@@ -3,8 +3,10 @@ import {LoginFormContainer} from '@/app/components/LoginPage/LoginFormContainer/
 
 export const LoginPage = () => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-2'>
-            <LoginIllustration className='hidden lg:block'/>
+        <div className='grid grid-cols-1 xs:grid-cols-1 xl:grid-cols-2 min-h-screen'>
+            <div className='hidden xl:block'>
+                <LoginIllustration/>
+            </div>
             <LoginFormContainer/>
         </div>
     );
